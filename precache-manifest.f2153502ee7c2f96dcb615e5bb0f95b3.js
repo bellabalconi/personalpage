dc9b8eb0b0067f479d608bc4b03b0ea920488a4e
Izabella <izabellabalconi@gmail.com>
2019-04-26 23:03:56 -0300

    Updates

---
+++ precache-manifest.f2153502ee7c2f96dcb615e5bb0f95b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63d5b4d0e080bebe6ab2",
+    "url": "/personalpage/static/css/main.ebffe120.chunk.css"
+  },
+  {
+    "revision": "63d5b4d0e080bebe6ab2",
+    "url": "/personalpage/static/js/main.d5aab668.chunk.js"
+  },
+  {
+    "revision": "9bf558f3b9d8e376819f",
+    "url": "/personalpage/static/js/runtime~main.1d9cd997.js"
+  },
+  {
+    "revision": "8868e4e38dc663c4f85b",
+    "url": "/personalpage/static/css/2.a99bb038.chunk.css"
+  },
+  {
+    "revision": "8868e4e38dc663c4f85b",
+    "url": "/personalpage/static/js/2.0b749df4.chunk.js"
+  },
+  {
+    "revision": "c7965a6055f79d5162b863d4f5c07f47",
+    "url": "/personalpage/static/media/8bits.c7965a60.png"
+  },
+  {
+    "revision": "aa20dae77ab50133a9e8de179a2a3aa5",
+    "url": "/personalpage/static/media/eu.aa20dae7.jpg"
+  },
+  {
+    "revision": "2233acc0593366f35056b098d1002c27",
+    "url": "/personalpage/index.html"
+  }
+];
